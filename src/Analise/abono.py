45a18893e1ca5bbee038f707fa57983ff9348b38
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-06-27 17:39:06 -0300

    chore: Refactor Abono class to use type hints and improve code readability

--- src/Analise/abono.py
+++ src/Analise/abono.py
@@ -6,28 +6,28 @@ class Abono:
         self.file_path = file_path
         self.abono_df = None
 
-    def load_data(self):
+    def load_data(self) -> None:
         self.abono_df = pd.read_csv(self.file_path, sep=";", encoding='latin1')
 
-    def get_anos(self):
+    def get_anos(self) -> pd.Series:
         return self.abono_df["Ano"].unique()
 
-    def get_meses(self, ano):
+    def get_meses(self, ano: str) -> pd.Series:
         return self.abono_df.loc[self.abono_df["Ano"] == ano, "Mes"].unique()
 
     def qtd_servidores_abono_permanencia_por_ano_mes(self, ano: str, mes: str) -> int:
         return self.abono_df.loc[(self.abono_df['Ano'] == ano) & (self.abono_df['Mes'] == mes)].shape[0]
 
-    def qtd_servidores_abono_permanencia_por_uf_residencia(self):
+    def qtd_servidores_abono_permanencia_por_uf_residencia(self) -> pd.Series:
         return self.abono_df.groupby("UF Residência").size()
 
-    def qtd_servidores_abono_permanencia_por_situacao_servidor(self):
+    def qtd_servidores_abono_permanencia_por_situacao_servidor(self) -> pd.Series:
         return self.abono_df.groupby("Situação servidor").size()
 
-    def qtd_servidores_abono_permanencia_por_uf_upag_vinculacao(self):
+    def qtd_servidores_abono_permanencia_por_uf_upag_vinculacao(self) -> pd.Series:
         return self.abono_df.groupby("UF da UPAG de vinculação").size()
 
-    def qtd_servidores_abono_permanencia_por_denominacao_unidade_organizacional(self):
+    def qtd_servidores_abono_permanencia_por_denominacao_unidade_organizacional(self) -> pd.Series:
         return self.abono_df.groupby("Denominação unidade").size()
 
     def qtd_servidores_abono_permanencia_por_cidade_residencia(self):
--- src/pages/Abono.py
+++ src/pages/Abono.py
@@ -3,12 +3,29 @@ from datetime import datetime
 
 import streamlit as st
 from streamlit_option_menu import option_menu
+
 import plotly.express as px
 from Analise.abono import Abono
+
 
 # Config
 st.set_page_config(page_title="Abono Permanência",
                    page_icon="🕗", layout="wide")
+
+
+st.markdown("""
+    <style>
+    div[data-testid="stMetric"] {
+        background-color: rgba(28, 131, 225, 0.1); /* Cor de fundo */
+        border: 1px solid rgba(28, 131, 225, 0.1); /* Borda */
+        padding: 5% 5% 5% 10%; /* Preenchimento */
+        border-radius: 5px; /* Bordas arredondadas */
+        color: rgb(30, 103, 119); /* Cor do texto */
+        overflow-wrap: break-word;
+    }
+
+    </style>
+""", unsafe_allow_html=True)
 
 
 def set_menu():
@@ -40,49 +57,112 @@ def show_dashboard_01(abono) -> None:
     mes_anterior = st.session_state['mes'] if 'mes' in st.session_state else None
 
     anos = abono.get_anos()
-    ano = col1.selectbox("ano", anos, placeholder="Ano", index=len(anos)-1,
-                         label_visibility="hidden")
+    ano = col2.selectbox(label="Ano", options=anos,
+                         placeholder="Ano", index=len(anos)-1)
 
     meses = abono.get_meses(ano)
-    mes: int = col2.selectbox("mês", abono.get_meses(ano), index=len(meses)-1, placeholder="Mês",
-                              label_visibility="hidden")
+    mes: int = col3.selectbox("Mês", abono.get_meses(
+        ano), index=len(meses)-1, placeholder="Mês")
 
     if ano and mes:
 
-        delta = 0
-        valor_anterior = 0
-        valor_atual = abono.qtd_servidores_abono_permanencia_por_ano_mes(ano, mes)
+        # delta = 0
+        # valor_anterior = 0
+        # valor_atual = abono.qtd_servidores_abono_permanencia_por_ano_mes(
+        #     ano, mes)
 
-        if ano != ano_anterior or mes != mes_anterior and ano_anterior and mes_anterior:
-            valor_anterior = abono.qtd_servidores_abono_permanencia_por_ano_mes(ano_anterior, mes_anterior)
-            if valor_anterior:
-                delta = ((valor_atual / valor_anterior) - 1) * 100
-                delta = f"{round(delta, 2)}%".replace(".", ",")
+        # if ano != ano_anterior or mes != mes_anterior and ano_anterior and mes_anterior:
+        #     valor_anterior = abono.qtd_servidores_abono_permanencia_por_ano_mes(
+        #         ano_anterior, mes_anterior)
+        #     if valor_anterior:
+        #         delta = ((valor_atual / valor_anterior) - 1) * 100
+        #         delta = f"{round(delta, 2)}%".replace(".", ",")
 
-        cols = st.columns(5)
+        # cols = st.columns(3)
 
+        # with cols[0]:
+        #     if ano_anterior and mes_anterior:
+        #         st.metric(label=f"Servidores com abono permanência em {str(mes_anterior).zfill(2)}/{ano_anterior}",
+        #                   value=valor_anterior)
+
+        # with cols[1]:
+        #     st.metric(label=f"Servidores com abono permanência em {str(mes).zfill(2)}/{ano}",
+        #               value=valor_atual, delta=delta)
+
+        # with cols[2]:
+
+        #     now = datetime.now()
+        #     ano_atual = now.year
+
+        #     ultimo_mes = abono.get_meses(ano_atual)[-1]
+
+        #     st.metric(label=f"Servidores com abono permanência em {ultimo_mes}/{ano_atual}",
+        #               value=abono.qtd_servidores_abono_permanencia_por_ano_mes(ano_atual, ultimo_mes))
+
+        # if ano != ano_anterior or mes != mes_anterior:
+        #     st.session_state['ano'] = ano
+        #     st.session_state['mes'] = mes
+
+        ###########################################################################################
+
+        # TODO: Separar para outro modulo a logica para renderizacao dos cards
+        # TODO: Adicionar a logica para o delta (variacao percentual mes anterior e proximo mes em relacao ao mes atual)
+
+        qtd_serv_mes_atual = abono.qtd_servidores_abono_permanencia_por_ano_mes(
+            ano, mes)
+        label_mes_anterior = None
+        label_proximo_mes = None
+
+        if ano == 2017 and mes == 1:
+            qtd_serv_mes_anterior = 0
+            label_mes_anterior = "Sem dados"
+
+        else:
+
+            if mes == 1:
+                qtd_serv_mes_anterior = abono.qtd_servidores_abono_permanencia_por_ano_mes(
+                    ano - 1, 12)
+                label_mes_anterior = f"Servidores com abono permanência em 12/{ano - 1}"
+
+            else:
+                qtd_serv_mes_anterior = abono.qtd_servidores_abono_permanencia_por_ano_mes(
+                    ano, max(mes - 1, 1))
+                label_mes_anterior = f"Servidores com abono permanência em {str(max(mes - 1, 1)).zfill(2)}/{ano if mes != 1 else ano - 1}"
+
+        if ano == 2024 and mes == 5:
+            qtd_serv_proximo_mes = 0
+            label_proximo_mes = "Sem dados"
+
+        else:
+
+            if mes == 12:
+                qtd_serv_proximo_mes = abono.qtd_servidores_abono_permanencia_por_ano_mes(
+                    ano + 1, 1)
+                label_proximo_mes = f"Servidores com abono permanência em 01/{ano + 1}"
+
+            else:
+                qtd_serv_proximo_mes = abono.qtd_servidores_abono_permanencia_por_ano_mes(
+                    ano, min(mes + 1, 12))
+                label_proximo_mes = f"Servidores com abono permanência em {str(min(mes + 1, 12)).zfill(2)}/{ano + 1 }"
+
+        cols = st.columns(3)
+        with cols[0]:
+            st.write("Mes anterior")
+            st.metric(label=label_mes_anterior,
+                      value=qtd_serv_mes_anterior)
         with cols[1]:
-            if ano_anterior and mes_anterior:
-                st.metric(label=f"Servidores com abono permanência em {str(mes_anterior).zfill(2)}/{ano_anterior}",
-                          value=valor_anterior)
-
-        with cols[2]:
+            st.write("Mes atual")
             st.metric(label=f"Servidores com abono permanência em {str(mes).zfill(2)}/{ano}",
-                      value=valor_atual, delta=delta)
+                      value=qtd_serv_mes_atual)
+        with cols[2]:
+            st.write("Mes seguinte")
+            st.metric(label=label_proximo_mes,
+                      value=qtd_serv_proximo_mes)
 
-        with cols[3]:
+    st.markdown("---")
 
-            now = datetime.now()
-            ano_atual = now.year
-
-            ultimo_mes = abono.get_meses(ano_atual)[-1]
-
-            st.metric(label=f"Servidores com abono permanência em {ultimo_mes}/{ano_atual}",
-                     value=abono.qtd_servidores_abono_permanencia_por_ano_mes(ano_atual, ultimo_mes))
-
-        if ano != ano_anterior or mes != mes_anterior:
-            st.session_state['ano'] = ano
-            st.session_state['mes'] = mes
+    # TODO: sessao para analise comparativa entre os anos
+    st.write("adicionar DateTimePicker para selecionar o intervalo de datas")
 
 
 def main() -> None:
@@ -98,13 +178,15 @@ def main() -> None:
 
     selected_page = set_menu()
 
-    home = os.getcwd() if 'home' not in st.session_state else st.session_state['home']
+    home = os.getcwd(
+    ) if 'home' not in st.session_state else st.session_state['home']
 
     abono = Abono(os.path.join(home, "src\\data\\geral.csv"))
     abono.load_data()
 
     # Seleção dinâmica da página
-    pages.get(selected_page, None)(abono) if pages.get(selected_page, None) else st.title("Página em construção")
+    pages.get(selected_page, None)(abono) if pages.get(
+        selected_page, None) else st.title("Página em construção")
 
 
 if __name__ == "__main__":
